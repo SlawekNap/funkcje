c0903f0dc65c2b2c16167b43be8beeef6a85da59
Slawomir Napiorkowski <napiorkowski.slawomir@gmail.com>
2022-01-27 14:30:59 +0100

    poprawa5

--- funkcje-palindrom.py
+++ funkcje-palindrom.py
@@ -1,16 +1,15 @@
-def check_palindrom(x):
+def check_palindrom(kajak):
     """ 
     Funkcja sprawdza czy dany zrot, 
     słowo zestaw cyfr itp. jest palidormem 
     tzn. czy dany zestaw znaków pisany na wspak
     jest taki sam jak pisany oryginalnie np
     kajak = kajak od tyłu
-    12sas21 = 12sas21
-    """
+    12sas21 = 12sas21 """
 
     x = input("Wpisz zwrot : ")
     return x == x[::-1] 
 
-result = check_palindrom("kajak")
+result = check_palindrom("")
 print(result)
 
